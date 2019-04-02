3bb944a14a0a92ea57847ca0882d5bedb642df7a
Uwana <uwikaiddi@gmail.com>
2019-04-01 20:59:56 -0500

    Install mathjs; install babel

--- index.js
+++ index.js
@@ -6,4 +6,23 @@ console.log('inside index.js');
 console.log('inside index.js and it is cool!');
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-console.log(math.sqrt(-4)) //2i
+console.log(math.sqrt(-4)); //2i
+
+var fibonacci = {
+  [Symbol.iterator]: function*() {
+    var pre = 0, cur = 1;
+    for (;;) {
+      var temp = pre;
+      pre = cur;
+      cur += temp;
+      yield cur;
+    }
+  }
+};
+
+for (var n of fibonacci) {
+  // truncate the sequence at 1000
+  if (n > 1000)
+    break;
+  console.log(n);
+}
