5da6f309c6c5eb26359a851131a9eac6e4ff8444
Uwana <uwikaiddi@gmail.com>
2019-04-01 20:33:17 -0500

    Install webpack

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-console.log('inside index.js');
+var moment = require('moment');
+var math = require('mathjs');
 
+
+console.log('inside index.js');
+console.log('inside index.js and it is cool!');
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+console.log(math.sqrt(-4)) //2i
